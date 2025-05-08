070ee1eb4e09dff0a45c953fbacd1c7f33e15092
Jay Kumar <jai.jk739@gmail.com>
2025-05-08 17:10:10 +1000

    Update server logic in app.js

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const app = express();
 const path = require('path');
 const homeRoutes = require('./routes/routes');
 
-// Set EJS view engine
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files (css, images)
+
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+
 app.use('/', homeRoutes);
 
-// Start server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
