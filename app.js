05eb446bbeaa077e4be96ec29f259c20ee59e5a0
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-05-10 00:08:08 +1000

    updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const homeRoutes = require('./routes/routes');
+const pagesRoutes   = require('./routes/pagesRoutes');
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,7 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', homeRoutes);
+// Route mounting
+app.use('/', authRoutes);
+app.use('/', productRoutes);
+app.use('/', cartRoutes);
+app.use('/', pagesRoutes);
 
 
 const PORT = process.env.PORT || 3000;
