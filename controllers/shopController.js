fa67a55fd0545a72a9a6ddbb3e0e6200debd3122
ISHIKA MANDAL <s223973381@deakin.edu.au>
2025-05-10 21:34:48 +1000

    added products in shop page

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -3,6 +3,8 @@ exports.getShop = (req, res) => {
   const cartItems = [
     { id: 1, name: "Nike Air Max", image:"/images/shoe1.jpg" , description: "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",  price: 120, quantity: 2, color: "black", size: 7 },
     { id: 2, name: "Adidas UltraBoost", image:"/images/shoe1.jpg",description: "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.", price: 150, quantity: 1, color: "red", size: 4 },
+    { id: 3, name: "Adidas UltraBoost2", image:"/images/shoe1.jpg",description: "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.", price: 150, quantity: 1, color: "red", size: 4 },
+    { id: 4, name: "Adidas UltraBoost3", image:"/images/shoe1.jpg",description: "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.", price: 150, quantity: 1, color: "red", size: 4 },
   ];
 
     res.render('shop', {
@@ -11,4 +13,5 @@ exports.getShop = (req, res) => {
       script: 'shop',
       cartItems
     });
+    console.error('Cart:', cartItems);
   };
